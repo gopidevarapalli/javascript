17f2d4689fd4ba5014cda18038cb254c539bc54d
Gopi Devarapalli <devarapalli.gopi8@gmail.com>
2019-11-07 11:31:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //There are 2 ways to create an array in javascript
 
-// 1
+// one way of creating an array
 let arr = new Array();
 arr = [3,2,4,5,4,6];  
 
-// 2
+// another way of creating an error
 
 let arr = [];
-arr = [3,2,4,5,4,6];  
+arr = [3,2,4,5,4,6];
+
+// Also you can create an array directly by assigning values.
+let arr = [3,2,4,5,4,6];  
 
